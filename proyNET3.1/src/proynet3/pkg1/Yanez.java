044d7d6a2582d6b730b221d9ceaedd4b94195510
alvar <alvar@DESKTOP-JHD2TLK>
2019-05-02 11:50:22 -0500

    sa1

---
+++ proyNET3.1/src/proynet3/pkg1/Yanez.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package proynet3.pkg1;
+
+/**
+ *
+ * @author alvar
+ */
+public class Yanez {
+    public static void main(String[] args) {
+        System.out.println("Holaaaaa"); //byalvaro
+    }
+    
+}
